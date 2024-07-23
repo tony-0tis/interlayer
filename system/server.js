9f9139b94b15b55755e5cd6599a3ed0504748736
Anton Ipatov <aipatov@igatec.com>
2024-07-23 22:55:23 +0300

    fix formidable

--- system/server.js
+++ system/server.js
@@ -1,4 +1,4 @@
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const { createServer: createHttpServer} = require('http');
 const { createServer: createHttpsServer} = require('https');
 const { parse: queryParse } = require('querystring');
